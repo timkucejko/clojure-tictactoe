c564095c4c2bc4250973dfefbf87ee4c9c5d9eb9
timkucejko <tim@kucejko.com>
2015-05-14 23:57:38 -0500

    loop over win-paths

--- tictactoe.clj
+++ tictactoe.clj
@@ -3,7 +3,9 @@
 (def winning-patterns [[0 1 2] [3 4 5] [6 7 8] [0 3 6] [1 4 7] [2 5 8] [0 4 8] [2 4 6]])
 
 (if (= (nth board 0) "y") "yes" "no")
-(for [x board] (println x))
+
+
+(for [win-path winning-patterns] (println win-path))
 
 (comment
 ways to win:
