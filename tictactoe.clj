97dc8a503f856c459a0149ad28ca8a4b40ef0807
timkucejko <tim@kucejko.com>
2015-05-15 00:19:17 -0500

    verify a diagonal win... it looks like we can verify a winner. now to automate the implementation of winning-patterns

--- tictactoe.clj
+++ tictactoe.clj
@@ -1,11 +1,11 @@
-(def board ["x" "y" "x" 3 4 5 6 7 8])
+(def board ["x" 1 2 3 "x" 5 6 7 "x"])
 
 (def winning-patterns [[0 1 2] [3 4 5] [6 7 8] [0 3 6] [1 4 7] [2 5 8] [0 4 8] [2 4 6]])
 
 (if (= (nth board 0) "y") "yes" "no")
 
 (def test-path ["x" 1 2])
-(if (= (nth board 0) (nth board 1) (nth board 2) "x") println "x wins the match!")
+(if (= (nth board 0) (nth board 4) (nth board 8) "x") (println "x wins the match!") (println "no winner yet"))
 
 ;;(for [win-path winning-patterns] (if (= nth board (nth win-path 0))))
 
