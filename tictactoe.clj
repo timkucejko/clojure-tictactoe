1fc3e84b5a9a9c405d3f70416d23ac346b6d78ea
timkucejko <tim@kucejko.com>
2015-05-14 23:49:00 -0500

    replace first board position with character and evaluate an if statement against it.

--- tictactoe.clj
+++ tictactoe.clj
@@ -1,9 +1,8 @@
-(def board [0 1 2 3 4 5 6 7 8])
+(def board ["x" 1 2 3 4 5 6 7 8])
 
 (def winning-patterns [[0 1 2] [3 4 5] [6 7 8] [0 3 6] [1 4 7] [2 5 8] [0 4 8] [2 4 6]])
 
-(def number 0)
-(if (= (nth board 0) 0) "yes" "no")
+(if (= (nth board 0) "y") "yes" "no")
 
 
 (comment
