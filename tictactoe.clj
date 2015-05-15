114b49ca6141034a12706be0d9113f032f0870d4
timkucejko <tim@kucejko.com>
2015-05-14 23:43:04 -0500

    add a working if statement to the mix

--- tictactoe.clj
+++ tictactoe.clj
@@ -2,7 +2,8 @@
 
 (def winning-patterns [[0 1 2] [3 4 5] [6 7 8] [0 3 6] [1 4 7] [2 5 8] [0 4 8] [2 4 6]])
 
-(if (< number 100) "yes" "no"))
+(def number 100)
+(if (= number 100) "yes" "no")
 
 
 (comment
